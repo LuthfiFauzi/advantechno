d34b5d86826b0b17cd9991dc46de54159b30a7a8
Luthfi Fauzi <faiz.uzi96@gmail.com>
2018-02-08 12:49:51 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Advanced Technology Facility</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/model.css">
+</head>
+
+<body class="bg-light">
+
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark py-2">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse right" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="#content">Introduction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="#cards">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="#aboutus">Contact Us</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0 pl-1">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-orange my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+  <section id="content">
+    <div class=" pt-6 bg-dark">
+      <h1 class="pl-5 text-white navbar-brand full-border-orange">Advanced Technology Facility</h1>
+    </div>
+
+    <div class="container-fluid bg-light py-5">
+      <div class="row">
+        <div class="col-md-6">
+          <div id="carouselIndicators" class="carousel slide  " data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner shadow-div-black">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/1.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Advanced Techno Facility</h5>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/2.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Advanced Techno Facility</h5>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/3.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Advanced Technology Facility</h5>
+                  <p>...</p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+          </div>
+        </div>
+        <div class="col-md-6 shadow-div-orange bg-white">
+          <h1 class="h1 pt-3">Introduction</h1>
+          <hr class="orange">
+          <div class="row">
+            <div class="col-md-6">
+              <p class="text-justify"> We are pleased to introduce ADVANCED TECHNOLOGY FACILITY PT, Register No 101114616882, a company specialized in providing services and support to the aviation organization such as provisioning for Aircraft Leasing, Purchase and Rental, Spare Parts as well as Repair / Overhaul of Fixed and Rotary Wing engines and accessories to the South East Asia, Asia Pacific, Middle East and the rest of the world.
+              <p class="text-justify">
+              Previously our name is ADVANCED TECHNOLOGY FACILITY CV, Reg No 101135215697, since 25 August 2011, we upgrade our company status to Company Limited (Perseroan Terbatas - PT) Reg No 10114616882 Incorporated and established since 2007 with more than 15 years experience in the industries, we have expanded our horizon to serve and help client to eliminate boundaries,  and collaborate in new ways, establishing trust and prompt response. </br>
+              
+            </div>
+            <div class="col-md-6">
+              <p class="text-justify">  
+               We are a Distributor of Aeronautical Products and In addition as a member of:
+                <li> Aviation Association Supplier </li>  
+                <li> The Chamber of Commerce and Industries </li>  
+                <li> The Association of middle Enterprise of Indonesia.</li></p>
+              <div class='text-justify'>
+              
+              We are globally expanding the full array of services to meet unique, local and regional needs. These cover the commercial and the aviation business sectors which include fixed and rotary wing. </p>
+              With strategic partners and alliances / collaboration with many local partners in Asia countries, many reputable various vendors, repair shops and manufactures of aeronautical products. We can be your source for all you needs under one roof   
+            </div> </p>  
+              ADVANCED TECHNOLOGY FACILITY, operate in:<br>
+              HEAD OFFICE </br>
+              Bandung - Indonesia </p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="aboutus">
+    <div class="container bg-light ">
+      <div class="row ">
+        <div class="col-md-6 py-4 bg-yellow  shadow-div-black">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="col-md-6 py-4 bg-dark text-white shadow-div-orange">
+          <p class="text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="cards">
+    <div class="container bg-light">
+        <div class="row">
+          <div class="col-md-4 py-5">
+            <div class="card  mx-auto shadow-div-orange" style="width: 18rem;">
+              <img class="card-img-top" src="assets/img/1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="">Read More >></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 py-5">
+            <div class="card  mx-auto shadow-div-orange" style="width: 18rem;">
+              <img class="card-img-top" src="assets/img/1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="">Read More >></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 py-5">
+            <div class="card mx-auto shadow-div-orange " style="width: 18rem;">
+              <img class="card-img-top" src="assets/img/1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="article.php?id=xx">Read More >></a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section id="break">
+    <div class="container bg-yellow shadow-div-black py-5">
+      <div class="row">
+        <div class="col-md-6 ">
+          <h1 class="h1">About Us</h1>
+          <hr class="black"> <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-6 ">
+          <h1 class="h1 text-right">Contact Us</h1>
+          <hr class="black "> <p class="text-justify">INDONESIA HEAD OFFICE </p>
+          Metro Trade Center Kav. B35 </br>
+        No. 590 Soekarno-Hatta Street </br> 
+        Bandung 40286 . Indonesia </br> 
+        Telp/Fax: +62.22.753.6432 </p> 
+        For general information you can send email to info@advantechno.com </p>
+        </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <div class="footer text-white pt-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="offset-md-11 offset-xs-11"></div>
+        <div class="col-md-1 col-xs-1">
+          <i class="fa fa-3x bg-dark fa-angle-up px-3 mr-2" aria-hidden="true" style="color:white;" id="scrollup"></i>
+        </div>
+      </div>
+      <div class="row bg-dark pt-2">
+        <div class="col-md-4 col-xs-4">
+          <p>
+            Follow : <i class="fa fa-facebook-square mr-1" aria-hidden="true"></i>advantechno
+          </p>
+
+        </div>
+        <div class="offset-md-4 offset-xs-4"></div>
+        <div class="col-md-4 col-xs-4">
+          <p class="text-right">&copy; Web Designed by XCALIBUR. All rights reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/scroll.js"></script>
+
+</html>
