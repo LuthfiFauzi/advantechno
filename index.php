75577b5ac8ab74686e1acb04768cf4427ba8e5c9
Luthfi Fauzi <faiz.uzi96@gmail.com>
2018-02-09 17:37:01 +0700

    aa
    aa

--- index.php
+++ index.php
@@ -3,29 +3,41 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Advanced Technology Facility</title>
+  
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/css/model.css">
 </head>
 
 <body class="bg-light">
-
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark py-2">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="collapse navbar-collapse right" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse right">
+      <div class="h3 pt-1">
+            <a style="color: white" href="#Home" > Advanced Technology Facility</a>
+          </div>
       <ul class="navbar-nav ml-auto">
+
         <li class="nav-item">
-          <a class="nav-link border-orange" href="#content">Introduction</a>
+          <a class="nav-link border-orange" href="#Home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-orange" href="#cards">Service</a>
+          <a class="nav-link border-orange" href="#Introduction">Introduction</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link border-orange" href="#aboutus">Contact Us</a>
+          <a class="nav-link border-orange" href="#Service">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="#Product">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="#Contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-orange" href="login.php">Log in</a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0 pl-1">
@@ -35,43 +47,37 @@
     </div>
   </nav>
 
-  <section id="content">
-    <div class=" pt-6 bg-dark">
-      <h1 class="pl-5 text-white navbar-brand full-border-orange">Advanced Technology Facility</h1>
-    </div>
-
-    <div class="container-fluid bg-light py-5">
-      <div class="row">
-        <div class="col-md-6">
-          <div id="carouselIndicators" class="carousel slide  " data-ride="carousel">
+          <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
               <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
               <li data-target="#carouselIndicators" data-slide-to="1"></li>
               <li data-target="#carouselIndicators" data-slide-to="2"></li>
             </ol>
+            <section id="Home">
             <div class="carousel-inner shadow-div-black">
               <div class="carousel-item active">
-                <img class="d-block w-100" src="assets/img/1.jpg" alt="First slide">
+                <img class="d-block w-100" src="assets/img/1.jpg" alt="First slide" height="600">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>Advanced Techno Facility</h5>
+                  <h5>Advanced Technology Facility</h5>
                   <p>...</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100" src="assets/img/2.jpeg" alt="Second slide">
+                <img class="d-block w-100" src="assets/img/2.jpeg" alt="Second slide" height="600">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>Advanced Techno Facility</h5>
+                  <h5>Advanced Technology Facility</h5>
                   <p>...</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100" src="assets/img/3.jpg" alt="Third slide">
+                <img class="d-block w-100" src="assets/img/3.jpg" alt="Third slide" height="600">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Advanced Technology Facility</h5>
                   <p>...</p>
                 </div>
               </div>
             </div>
+            </section>
             <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
@@ -79,20 +85,28 @@
             <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
+          
         </a>
           </div>
         </div>
-        <div class="col-md-6 shadow-div-orange bg-white">
-          <h1 class="h1 pt-3">Introduction</h1>
-          <hr class="orange">
-          <div class="row">
-            <div class="col-md-6">
+      </div>
+    </div>
+  </section>
+
+    </p>
+        <section id="Introduction">
+        <div class="col-md-12 shadow-div-orange bg-white">
+          <h1 class="h1 pt-3"><center>Introduction</center></h1>
+          <hr class="bg-yellow" width="1150" > 
+  <section id="">
+    <div class="container bg-light ">
+      <div class="row ">
+        <div class="col-md-6 py-4 ">
               <p class="text-justify"> We are pleased to introduce ADVANCED TECHNOLOGY FACILITY PT, Register No 101114616882, a company specialized in providing services and support to the aviation organization such as provisioning for Aircraft Leasing, Purchase and Rental, Spare Parts as well as Repair / Overhaul of Fixed and Rotary Wing engines and accessories to the South East Asia, Asia Pacific, Middle East and the rest of the world.
               <p class="text-justify">
               Previously our name is ADVANCED TECHNOLOGY FACILITY CV, Reg No 101135215697, since 25 August 2011, we upgrade our company status to Company Limited (Perseroan Terbatas - PT) Reg No 10114616882 Incorporated and established since 2007 with more than 15 years experience in the industries, we have expanded our horizon to serve and help client to eliminate boundaries,  and collaborate in new ways, establishing trust and prompt response. </br>
-              
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 py-4">
               <p class="text-justify">  
                We are a Distributor of Aeronautical Products and In addition as a member of:
                 <li> Aviation Association Supplier </li>  
@@ -109,21 +123,33 @@
               
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </section>
+        </div></p></p> 
 
-  <section id="aboutus">
-    <div class="container bg-light ">
-      <div class="row ">
-        <div class="col-md-6 py-4 bg-yellow  shadow-div-black">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <section id="Service">
+          <h1 class="h1 pt-3" style="background-color: white">
+            <center>Our Service</center></h1>
+            <div class="container bg-light ">
+              <hr class="bg-yellow" style="">
+              <div class="row ">
+        <div class="col-md-6 py-4 ">
+          <p class="text-justify">
+          <b> SPARES AIRCRAFT COMPONETS SUPPORTED </b> </p>
+          To give best support for our customers, we believe that our expertise must cover several aircrafts types. We can aim to provide total support for our customers needs. We can provide spares support especially for the following aircraft types
+          <li>Bell Helicopter all series</li>
+          <li>Fokker 27, 28, 100</li>
+          <li>Boeing 727/737/747 Series</li>
+          <li>Airbus A300-A330 Series</li>
+          <li>NC212,CN235 and others Indonesia aircraft products</li></p>
+          <p class="text-justify">We concern to keep in touch with our customers fleet, we buy stock and surplus packages on regular basis. If you do not see your fleet type above. Please contact us so we can inform you of our stock latest holding.</p>
         </div>
-        <div class="col-md-6 py-4 bg-dark text-white shadow-div-orange">
-          <p class="text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="col-md-6 py-4">
+          <p class="text-justify">
+            <b>REPAIR AND SERVICES</b> </p>
+            We can offer third party repair and overhaul management service. We elaborate with our thrust partners either manufactures or approved repair shops to provide best quality product with competitive price and reasonable turn around time. </p> </p>
+            <b>OTHER AIRCRAFT OPERATION SUPPORT SERVICES</b> </p>
+            Aircraft and Engines maintenance support Aircraft and Engines purchasing, selling and leasing support Most of the above products comes traceability or manufacturer recommended Shelf Life, technical support data and product labels upon purchase.</p>
+            We can be your source for all you needs under one roof 
+            
         </div>
       </div>
     </div>
@@ -155,6 +181,7 @@
               <img class="card-img-top" src="assets/img/1.jpg" alt="Card image cap">
               <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
                 <a href="article.php?id=xx">Read More >></a>
               </div>
             </div>
@@ -163,7 +190,7 @@
     </div>
   </section>
 
-  <section id="break">
+  <section id="Contact">
     <div class="container bg-yellow shadow-div-black py-5">
       <div class="row">
         <div class="col-md-6 ">
@@ -203,7 +230,7 @@
         </div>
         <div class="offset-md-4 offset-xs-4"></div>
         <div class="col-md-4 col-xs-4">
-          <p class="text-right">&copy; Web Designed by XCALIBUR. All rights reserved</p>
+          <p class="text-right">&copy; Web Designed by ATF. All rights reserved</p>
         </div>
       </div>
     </div>
